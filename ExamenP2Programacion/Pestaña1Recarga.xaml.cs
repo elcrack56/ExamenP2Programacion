3b6f0e6e90e136bbdabf1c54c2e2cb9e91db88d0
elcrack56 <darwinsteven3@gmail.com>
2024-11-29 07:57:44 -0500

    generacion de los tabs

---
+++ ExamenP2Programacion/Pestaña1Recarga.xaml.cs
@@ -0,0 +1,9 @@
+namespace ExamenP2Programacion;
+
+public partial class Pestaña1Recarga : ContentPage
+{
+	public Pestaña1Recarga()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ExamenP2Programacion/Pestaña2GridNombre.xaml.cs
@@ -0,0 +1,9 @@
+namespace ExamenP2Programacion;
+
+public partial class Pestaña2GridNombre : ContentPage
+{
+	public Pestaña2GridNombre()
+	{
+		InitializeComponent();
+	}
+}
