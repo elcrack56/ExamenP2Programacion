5218531c94820e59bbb209bb4c4adf101f0b2e5c
laboratorio <laboratorio@UPOAULA10610>
2024-11-29 07:32:26 -0500

    Agregar archivos de proyecto.

---
+++ ExamenP2Programacion/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamenP2Programacion
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ExamenP2Programacion/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExamenP2Programacion
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
